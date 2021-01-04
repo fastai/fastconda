03ca64fdba39daf54ed6cde9aaefd63ad2a18541
Jeremy Howard <j@fast.ai>
2021-01-04 12:34:54 -0800

    true

--- build_conda.sh
+++ build_conda.sh
@@ -10,5 +10,5 @@ git clone https://github.com/rwightman/pytorch-image-models.git --depth 1
 cd pytorch-image-models
 setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch .
 conda convert -p all -o conda_packages conda_packages/linux-64/timm-0.3.3-py38_0.tar.bz2
-anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true
 
