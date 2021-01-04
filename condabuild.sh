c62516445d21af23e37b8767f64d83346c1dfb40
Jeremy Howard <j@fast.ai>
2021-01-04 11:44:13 -0800

    activate

--- condabuild.sh
+++ condabuild.sh
@@ -7,5 +7,6 @@ conda create -yqn tmpbuild python=$1 anaconda-client conda-build
 conda activate tmpbuild
 conda build sentencepiece
 conda build purge
+conda activate
 anaconda -t $ANACONDA_TOKEN upload --skip -u fastai $CONDA_PREFIX/conda-bld/*/sentencepiece-*.tar.bz2
 
