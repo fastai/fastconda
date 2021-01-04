63c4feff669909c77272387f60bb8b0fbf5f8c4a
Jeremy Howard <j@fast.ai>
2021-01-04 12:43:50 -0800

    true

--- condabuild.sh
+++ condabuild.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir -p output
 conda build sentencepiece --output-folder output
-anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2 || true
 
