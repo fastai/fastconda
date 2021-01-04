0508da95271634e6aa5ba3a17a45266ee0b33bf6
Jeremy Howard <j@fast.ai>
2021-01-04 12:28:02 -0800

    build

--- condabuild.sh
+++ condabuild.sh
@@ -1,12 +1,7 @@
-#! /usr/bin/env bash
+#! /bin/bash -l
 set -e
-: ${1?"Usage: $0 version"}
 
-PATH=${CONDA:=$CONDA_PREFIX}/condabin:$PATH
-eval "$(conda shell.bash hook)"
-conda create -yqn tmpbuild python=$1 anaconda-client conda-build
-conda activate tmpbuild
-conda build sentencepiece
-conda build purge
-anaconda -t $ANACONDA_TOKEN upload --skip -u fastai $(conda info --root)/conda-bld/*/sentencepiece-*.tar.bz2
+mkdir -p output
+conda build sentencepiece --output-folder output
+anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2
 
