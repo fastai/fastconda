03b31ca6b8a3591601bc6c406bc559e0427f6a45
Jeremy Howard <j@fast.ai>
2021-01-03 17:10:08 -0800

    build_conda

---
+++ build_conda.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+set -e
+
+eval "$(conda shell.bash hook)"
+conda create -yqn tmp-buildconda -c cbillington -c defaults python=3.8 setuptools-conda anaconda-client
+conda activate tmp-buildconda
+mkdir -p tmp && cd tmp
+
+git clone https://github.com/rwightman/pytorch-image-models.git --depth 1
+cd pytorch-image-models
+setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch .
+conda convert -p all -o conda_packages conda_packages/linux-64/timm-0.3.3-py38_0.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2
+
--- pypi2conda.py
+++ pypi2conda.py
@@ -5,8 +5,7 @@ from fastcore.all import *
 @call_parse
 def main(name:Param("Name of pypi lib",str)):
     ver = update_meta(name, f'{name}/meta.yaml.tmpl', f'{name}/meta.yaml')
-    with open('timm/build.log', 'w') as f:
-        f.write(run('conda mambabuild timm'))
-        f.write('-----------------------------------')
-        f.write(anaconda_upload(name, ver, 'fastai'))
+    print(run('conda mambabuild timm'))
+    print('-----------------------------------')
+    print(anaconda_upload(name, ver, 'fastai'))
 
