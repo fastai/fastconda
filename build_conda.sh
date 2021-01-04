a8cbfef2c039dfbd7f0dbbc201e9ef47d5410831
hamelsmu <hamel.husain@gmail.com>
2021-01-04 14:27:26 -0800

    minor nits

--- build_conda.sh
+++ build_conda.sh
@@ -8,7 +8,7 @@ if [ "$tag" ]; then
     conda activate tmp-buildconda
     mkdir -p tmp && cd tmp
 
-    git clone -b $tag --depth 1 https://github.com/rwightman/pytorch-image-models.git .
+    git clone -b "$tag" --depth 1 https://github.com/rwightman/pytorch-image-models.git .
     setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch .
     conda convert -p all -o conda_packages conda_packages/*/*.tar.bz2
     anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2
