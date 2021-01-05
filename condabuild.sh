a4fcac8da3c8652fd1c0c4cf63ba3917f36a0ca0
hamelsmu <hamel.husain@gmail.com>
2021-01-05 09:06:25 -0800

    group logs

--- condabuild.sh
+++ condabuild.sh
@@ -1,7 +1,11 @@
 #! /bin/bash -l
 set -e
 
+echo "::group::Build Packages"
 mkdir -p output
 conda build sentencepiece --output-folder output
-anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2 || true
+echo "::endgroup::"
 
+echo "::group::Upload Packages"
+anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2 || true
+echo "::endgroup::"
