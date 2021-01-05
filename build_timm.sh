2c4cf6db9c11b80bdd22abc15641e5489d69f55c
hamelsmu <hamel.husain@gmail.com>
2021-01-04 16:25:16 -0800

    fix timm

--- build_timm.sh
+++ build_timm.sh
@@ -10,13 +10,10 @@ fi
 
 tag=$(python get_tag.py "$1")
 if [ "$tag" ]; then
-    eval "$(conda shell.bash hook)"
-    conda create -yqn tmp-buildconda -c cbillington -c defaults python=3.8 setuptools-conda anaconda-client
-    conda activate tmp-buildconda
+    mamba install -yq -c cbillington -c defaults setuptools-conda anaconda-client
     mkdir -p tmp && cd tmp
-
     git clone -b "$tag" --depth 1 https://github.com/rwightman/pytorch-image-models.git .
     setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch .
     conda convert -p all -o conda_packages conda_packages/*/*.tar.bz2
-    anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true
+    anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true;
 fi
