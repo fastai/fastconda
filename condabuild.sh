a6faf6f89cd53023736c0d154a6978284751e7ec
hamelsmu <hamel.husain@gmail.com>
2021-01-05 10:31:12 -0800

    update ci scripts

--- condabuild.sh
+++ condabuild.sh
@@ -1,11 +1,16 @@
 #! /bin/bash -l
 set -e
+tag=$(python get_tag.py --nm sentencepiece --apkg sentencepiece)
 
-echo "::group::Build Packages"
-mkdir -p output
-conda build sentencepiece --output-folder output
-echo "::endgroup::"
+if [ "$tag" ]; then
+    echo "::group::Build Package"
+    mkdir -p output
+    conda build sentencepiece --output-folder output
+    echo "::endgroup::"
 
-echo "::group::Upload Packages"
-anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2 || true
-echo "::endgroup::"
+    echo "::group::Upload Package"
+    anaconda -t $ANACONDA_TOKEN upload --skip -u fastai output/*/sentencepiece-*.tar.bz2 || true
+    echo "::endgroup::"
+else
+    echo "Conda package fastai/sentencepiece already up to date."
+fi
--- get_tag.py
+++ get_tag.py
@@ -1,16 +1,15 @@
 from ghapi.all import *
 from fastcore.all import *
 from packaging import version
+from fastrelease.conda import *
 
 @call_parse
-def get_new_gh_tag(ghowner:Param('GitHub Owner', str)='rwightman', 
-                   ghrepo:Param('GitHub Repo', str)='pytorch-image-models', 
-                   achan:Param('Anaconda Channel', str)='fastai', 
-                   apkg:Param('Anaconda Package', str)='timm',
-                   force:Param('Return the github tag no matter what.', store_true)=False):
-    "Returns GitHub tag only if a newer release exists compared to an Anaconda Repo."
-    ghtagnm = GhApi().repos.get_latest_release(ghowner, repo=ghrepo).tag_name
-    ghtag = version.parse(ghtagnm)
+def new_gh_tag(nm:Param('Package name on pypi', str)='timm', 
+               achan:Param('Anaconda Channel', str)='fastai', 
+               apkg:Param('Anaconda Package', str)='timm',
+               force:Param('Return the github tag no matter what.', store_true)=False):
+    "Prints GitHub tag only if a newer release exists on Pypi compared to an Anaconda Repo."
+    pypitag = latest_pypi(nm)
     condavs = L(loads(run(f'mamba repoquery search {apkg} -c {achan} --json'))['result']['pkgs'])
     condatag = max(condavs.attrgot('version').map(version.parse))
-    if ghtag > condatag or force: print(ghtagnm)
+    if pypitag > condatag or force: print('v'+str(pypitag))
