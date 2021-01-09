cf49d44c299f6cffe74fa19dbd3b93a5dde40de7
Jeremy Howard <j@fast.ai>
2021-01-08 16:46:05 -0800

    remove chk

--- condabuild.py
+++ condabuild.py
@@ -7,7 +7,6 @@ import shutil
 @call_parse
 def main(name:Param("Name of pypi lib",str),
          args:Param("Extra args to pass to `build`",str)=''):
-    if not chk_conda_rel(name): return print('Package already up to date')
     ver = update_meta(name, f'{name}/meta.yaml.tmpl', f'{name}/meta.yaml')
     with actions_group(f'Build {name}'): print(run(f'conda mambabuild {name} {args}'))
     with actions_group(f'Upload {name}'): print(anaconda_upload(name, ver, env_token='ANACONDA_TOKEN'))
