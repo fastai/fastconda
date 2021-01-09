8f64fb2e480fd89e46c95a17b35ed6cfe83e937d
Jeremy Howard <j@fast.ai>
2021-01-09 10:01:23 -0800

    channels

--- build_timm.sh
+++ build_timm.sh
@@ -5,7 +5,7 @@ tag=$(python get_tag.py "$1")
 if [ "$tag" ]; then
   rm -rf tmp && mkdir -p tmp && cd tmp
   git clone -b "$tag" --depth 1 https://github.com/rwightman/pytorch-image-models.git .
-  setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch --noarch .
+  setuptools-conda build --noarch .
   anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true;
 else
   echo no new release available
