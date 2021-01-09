c790c55bc22db56847f536624e52fb17dce03547
Jeremy Howard <j@fast.ai>
2021-01-09 10:00:15 -0800

    channels

--- build_timm.sh
+++ build_timm.sh
@@ -3,7 +3,6 @@ set -e
 
 tag=$(python get_tag.py "$1")
 if [ "$tag" ]; then
-  mamba install -yq -c cbillington -c defaults setuptools-conda anaconda-client
   rm -rf tmp && mkdir -p tmp && cd tmp
   git clone -b "$tag" --depth 1 https://github.com/rwightman/pytorch-image-models.git .
   setuptools-conda build --conda-name-differences 'torch:pytorch' -c pytorch --noarch .
