ef0fb3ec189ca7eb5446cf719d6ee988738cdbca
Jeremy Howard <j@fast.ai>
2021-01-10 09:59:45 -0800

    cd

--- setupconda.sh
+++ setupconda.sh
@@ -5,8 +5,9 @@ tag=$(python get_tag.py "$2" --pre "$3")
 if [ "$tag" ]; then
   rm -rf tmp
   git clone -b "$tag" --depth 1 https://github.com/"$1".git tmp
-  setuptools-conda build tmp
-  anaconda -t $ANACONDA_TOKEN upload --skip -u fastai tmp/conda_packages/*/*.tar.bz2 || true;
+  cd tmp
+  setuptools-conda build .
+  anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true;
 else
   echo no new release available
 fi
