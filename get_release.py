1561b42f83b53524d5ac41a839c57050940385bc
hamelsmu <hamel.husain@gmail.com>
2021-01-14 19:21:03 -0800

    take out write license

--- get_release.py
+++ get_release.py
@@ -16,10 +16,8 @@ def get_pypi_source(pypinm,
                     force:Param('Return the github tag no matter what.', store_true)=False):
     "Download latest source from pypi and untars to `dest` if a newer release exists on Pypi compared to an Anaconda Repo."
     dest = ifnone(dest,pypinm)
-    home_page = pypi_json('timm')['info']['home_page']
     pypi_ver,url,_ = pypi_details(pypinm)
     conda_ver = latest_conda(achan=achan, apkg=ifnone(apkg, pypinm))
     if force or not conda_ver or version.parse(pypi_ver) > conda_ver:
         with urlopen(url) as f: untar_dir(f, dest)
-        p = Path(dest)/'LICENSE.md'
-        if not p.exists(): p.write_text(f'For license information see {home_page}')
+
