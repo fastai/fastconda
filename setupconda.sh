7be89db1b7c283893165e3d9e28a06665ce3647f
hamelsmu <hamel.husain@gmail.com>
2021-01-14 19:20:49 -0800

    fix license issue

--- setupconda.sh
+++ setupconda.sh
@@ -2,11 +2,11 @@
 set -e
 
 rm -rf tmp
-python get_release.py $1 --dest 'tmp'
+python get_release.py $1 --dest 'tmp' --force
 
 if [ -d "tmp" ]; then  
   cd tmp
-  setuptools-conda build $2
+  setuptools-conda build --license-file 'None' $2
   anaconda -t $ANACONDA_TOKEN upload --skip -u fastai conda_packages/*/*.tar.bz2 || true;
 else
   echo "no new release available"
