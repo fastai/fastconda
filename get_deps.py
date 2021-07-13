7035c2462a3ad0808ab09b53a1b0a8b1263914b3
Jeremy Howard <j@fast.ai>
2021-07-12 21:00:22 -0700

    torchaudio

--- get_deps.py
+++ get_deps.py
@@ -24,7 +24,7 @@ if __name__=='__main__':
         links += inst("-c rapidsai -c nvidia -c defaults -c conda-forge 'cudf>=0.17' 'cudatoolkit>=11' mamba")
     links += L(
         "boa rich anaconda-client",
-        "'pytorch>=1.7' 'torchvision>0.7' pynvml",
+        "'pytorch>=1.7' torchaudio 'torchvision>0.7' pynvml",
         "-c huggingface -c pytorch -c nvidia -c defaults -c conda-forge transformers datasets accelerate",
         "sentencepiece 'spacy>=3.1' fastai timm",
         "nbdev fastrelease ghapi fastcgi",
