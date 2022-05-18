9c6a42b836cbb7718d48227442e1505849bd457e
Jeremy Howard <j@fast.ai>
2022-05-19 08:50:09 +1000

    Update get_deps.py

--- get_deps.py
+++ get_deps.py
@@ -29,7 +29,7 @@ if __name__=='__main__':
         "'pytorch>=1.7' torchaudio 'torchvision>0.7' pynvml",
         "-c huggingface -c pytorch -c nvidia -c defaults -c conda-forge transformers datasets accelerate",
         "sentencepiece 'spacy>=3.1' fastai timm",
-        "nbdev fastrelease ghapi fastcgi"
+        "nbdev fastrelease ghapi fastcgi fastbook"
         # "-c fastai -c defaults --override-channels albumentations",
         # "-c cbillington -c defaults setuptools-conda"
     ).map(inst).concat()
