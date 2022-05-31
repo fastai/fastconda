cb4a23ab5b542a1f82c8158644fb90ffcda8c91f
Jeremy Howard <j@fast.ai>
2022-06-01 06:06:01 +1000

    forge

--- get_deps.py
+++ get_deps.py
@@ -5,13 +5,13 @@ import platform
 def inst(s, bin='mamba'):
     cmd = f'{bin} install --json -qy {s}'
     #cmd = f'{bin} install --strict-channel-priority --json -qy {s}'
-    print(cmd)
+    print('***', cmd)
     out = run(cmd)
     try: res = dict2obj(loads(out))
     except:
         print(out)
         raise
-    #print(out)
+    #print('---', out)
     return nested_attr(res, 'actions.LINK')
 
 tok = os.getenv('FASTCHAN')
@@ -21,16 +21,19 @@ def anacopy(nm):
     if out.strip(): print(out.strip())
 
 if __name__=='__main__':
-    links = L(inst('mamba', 'conda'))
-    #if sys.version_info[:2] not in ((3,6),(3,9)) and platform.system()=='Linux':
+    chans = '-c pytorch -c fastai -c conda-forge'
+    links = L([]) #L(inst('mamba', 'conda'))
+    if platform.system() in ('Linux','Windows'):
+        links += inst(f"{chans} 'cudatoolkit>=11'")
     #    links += inst("-c rapidsai -c nvidia -c defaults -c conda-forge 'cudf>=0.17' 'cudatoolkit>=11' mamba")
     links += L(
-        "boa rich anaconda-client",
-        "'pytorch>=1.7' torchaudio 'torchvision>0.7' pynvml",
-        "-c huggingface -c pytorch -c nvidia -c defaults -c conda-forge transformers datasets accelerate",
-        "sentencepiece 'spacy>=3.1' fastai timm",
-        "nbdev fastrelease ghapi fastcgi fastbook"
-        # "-c fastai -c defaults --override-channels albumentations",
+        f"{chans} mamba boa rich anaconda-client",
+        f"{chans} 'pytorch>=1.7' torchaudio 'torchvision>0.7' pynvml",
+        f"{chans} transformers datasets accelerate",
+        f"{chans} sentencepiece 'spacy>=3.1' fastai timm",
+        f"{chans} nbdev fastrelease ghapi fastcgi fastbook",
+        f"{chans} albumentations",
+        f"{chans} setuptools-conda"
         # "-c cbillington -c defaults setuptools-conda"
     ).map(inst).concat()
 
